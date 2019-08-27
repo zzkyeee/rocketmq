3ad2d7d1eaf80a27067e6a4ed92a3f3ba5714c5c
zhangzhikai <zhangzhikai@corp.netease.com>
2019-08-28 00:27:56 +0800

    Add nameserver address at quick start

--- example/src/main/java/org/apache/rocketmq/example/quickstart/Producer.java
+++ example/src/main/java/org/apache/rocketmq/example/quickstart/Producer.java
@@ -44,13 +44,13 @@ public class Producer {
          * }
          * </pre>
          */
-
+        producer.setNamesrvAddr("127.0.0.1:9876");
         /*
          * Launch the instance.
          */
         producer.start();
 
-        for (int i = 0; i < 1000; i++) {
+        for (int i = 0; i < 10; i++) {
             try {
 
                 /*
